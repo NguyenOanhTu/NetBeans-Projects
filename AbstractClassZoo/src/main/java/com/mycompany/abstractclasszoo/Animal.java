0968a42380763b3106828220903c2f435c5a029c
NguyenOanhTu <164465527+NguyenOanhTu@users.noreply.github.com>
2024-03-28 22:36:40 +0700

    Add files via upload

---
+++ AbstractClassZoo/src/main/java/com/mycompany/abstractclasszoo/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.abstractclasszoo;
+
+/**
+ *
+ * @author oanht
+ */
+public abstract class Animal {
+    private String name;
+    private int age;
+    
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+}
